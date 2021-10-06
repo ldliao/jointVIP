245cbbb87e4999b2ffb7df5bd20ecc774a9672c5
ldliao <laura60629@gmail.com>
2021-10-05 22:55:36 -0700

    first compiled and loaded

--- tests/testthat/test-fbind.R
+++
@@ -1,9 +0,0 @@
-test_that("fbind() binds factor (or character)", {
-  x <- c("a", "b")
-  x_fact <- factor(x)
-  y <- c("c", "d")
-  z <- factor(c("a", "b", "c", "d"))
-
-  testthat::expect_identical(fbind(x, y), z)
-  testthat::expect_identical(fbind(x_fact, y), z)
-})
